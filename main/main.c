8d3146239a3b226d10e2c4a8269f3725ee6ddf32
Michael Dokolin <m@dokol.in>
2022-03-13 08:43:12 +0100

    Add station mode support

--- main/main.c
+++ main/main.c
@@ -1,4 +1,4 @@
-/* eth2ap (Ethernet to Wi-Fi AP packet forwarding) Example
+/* eth2wlan (Ethernet to Wi-Fi packet forwarding)
 
    This example code is in the Public Domain (or CC0 licensed, at your option.)
 
@@ -23,11 +23,12 @@
 #include "driver/spi_master.h"
 #endif
 
-static const char *TAG = "eth_example";
+static const char *TAG = "eth2wlan";
 static esp_eth_handle_t s_eth_handle = NULL;
 static QueueHandle_t flow_control_queue = NULL;
 static bool s_sta_is_connected = false;
 static bool s_ethernet_is_connected = false;
+static bool s_mac_is_set = false;
 static uint8_t s_eth_mac[6];
 
 #define FLOW_CONTROL_QUEUE_TIMEOUT_MS (100)
@@ -79,11 +80,28 @@ static void eth2wifi_flow_control_task(void *args)
     while (1) {
         if (xQueueReceive(flow_control_queue, &msg, pdMS_TO_TICKS(FLOW_CONTROL_QUEUE_TIMEOUT_MS)) == pdTRUE) {
             timeout = 0;
-            if (s_sta_is_connected && msg.length) {
+            if (msg.length) {
                 do {
+                    if (!s_mac_is_set) {
+                        memcpy(s_eth_mac, (uint8_t*)msg.packet + 6, sizeof(s_eth_mac));
+                        ESP_ERROR_CHECK(esp_wifi_start());
+                        #if CONFIG_WLAN_AP_MODE
+                        esp_wifi_set_mac(WIFI_IF_AP, s_eth_mac);
+                        #else
+                        esp_wifi_set_mac(WIFI_IF_STA, s_eth_mac);
+                        esp_wifi_connect();
+                        #endif
+                        s_mac_is_set = true;
+                    }
                     vTaskDelay(pdMS_TO_TICKS(timeout));
                     timeout += 2;
-                    res = esp_wifi_internal_tx(WIFI_IF_AP, msg.packet, msg.length);
+                    if (s_sta_is_connected) {
+                        #if CONFIG_WLAN_AP_MODE
+                        res = esp_wifi_internal_tx(WIFI_IF_AP, msg.packet, msg.length);
+                        #else
+                        res = esp_wifi_internal_tx(WIFI_IF_STA, msg.packet, msg.length);
+                        #endif
+                    }
                 } while (res && timeout < FLOW_CONTROL_WIFI_SEND_TIMEOUT_MS);
                 if (res != ESP_OK) {
                     ESP_LOGE(TAG, "WiFi send packet failed: %d", res);
@@ -104,8 +122,6 @@ static void eth_event_handler(void *arg, esp_event_base_t event_base,
         ESP_LOGI(TAG, "Ethernet Link Up");
         s_ethernet_is_connected = true;
         esp_eth_ioctl(s_eth_handle, ETH_CMD_G_MAC_ADDR, s_eth_mac);
-        esp_wifi_set_mac(WIFI_IF_AP, s_eth_mac);
-        ESP_ERROR_CHECK(esp_wifi_start());
         break;
     case ETHERNET_EVENT_DISCONNECTED:
         ESP_LOGI(TAG, "Ethernet Link Down");
@@ -129,24 +145,35 @@ static void wifi_event_handler(void *arg, esp_event_base_t event_base,
 {
     static uint8_t s_con_cnt = 0;
     switch (event_id) {
-    case WIFI_EVENT_AP_STACONNECTED:
-        ESP_LOGI(TAG, "Wi-Fi AP got a station connected");
-        if (!s_con_cnt) {
+        case WIFI_EVENT_AP_STACONNECTED:
+            ESP_LOGI(TAG, "Wi-Fi AP got a station connected");
+            if (!s_con_cnt) {
+                s_sta_is_connected = true;
+                esp_wifi_internal_reg_rxcb(WIFI_IF_AP, pkt_wifi2eth);
+            }
+            s_con_cnt++;
+            break;
+        case WIFI_EVENT_AP_STADISCONNECTED:
+            ESP_LOGI(TAG, "Wi-Fi AP got a station disconnected");
+            s_con_cnt--;
+            if (!s_con_cnt) {
+                s_sta_is_connected = false;
+                esp_wifi_internal_reg_rxcb(WIFI_IF_AP, NULL);
+            }
+            break;
+        case WIFI_EVENT_STA_CONNECTED:
+            ESP_LOGI(TAG, "Wi-Fi station connected");
             s_sta_is_connected = true;
-            esp_wifi_internal_reg_rxcb(WIFI_IF_AP, pkt_wifi2eth);
-        }
-        s_con_cnt++;
-        break;
-    case WIFI_EVENT_AP_STADISCONNECTED:
-        ESP_LOGI(TAG, "Wi-Fi AP got a station disconnected");
-        s_con_cnt--;
-        if (!s_con_cnt) {
+            esp_wifi_internal_reg_rxcb(WIFI_IF_STA, pkt_wifi2eth);
+            break;
+        case WIFI_EVENT_STA_DISCONNECTED:
+            ESP_LOGI(TAG, "Wi-Fi station disconnected");
             s_sta_is_connected = false;
-            esp_wifi_internal_reg_rxcb(WIFI_IF_AP, NULL);
-        }
-        break;
-    default:
-        break;
+            esp_wifi_internal_reg_rxcb(WIFI_IF_STA, NULL);
+            esp_wifi_connect();
+            break;
+        default:
+            break;
     }
 }
 
@@ -155,77 +182,77 @@ static void initialize_ethernet(void)
     ESP_ERROR_CHECK(esp_event_handler_register(ETH_EVENT, ESP_EVENT_ANY_ID, eth_event_handler, NULL));
     eth_mac_config_t mac_config = ETH_MAC_DEFAULT_CONFIG();
     eth_phy_config_t phy_config = ETH_PHY_DEFAULT_CONFIG();
-    phy_config.phy_addr = CONFIG_EXAMPLE_ETH_PHY_ADDR;
-    phy_config.reset_gpio_num = CONFIG_EXAMPLE_ETH_PHY_RST_GPIO;
-#if CONFIG_EXAMPLE_USE_INTERNAL_ETHERNET
+    phy_config.phy_addr = CONFIG_ETH_PHY_ADDR;
+    phy_config.reset_gpio_num = CONFIG_ETH_PHY_RST_GPIO;
+#if CONFIG_USE_INTERNAL_ETHERNET
     eth_esp32_emac_config_t esp32_emac_config = ETH_ESP32_EMAC_DEFAULT_CONFIG();
-    esp32_emac_config.smi_mdc_gpio_num = CONFIG_EXAMPLE_ETH_MDC_GPIO;
-    esp32_emac_config.smi_mdio_gpio_num = CONFIG_EXAMPLE_ETH_MDIO_GPIO;
+    esp32_emac_config.smi_mdc_gpio_num = CONFIG_ETH_MDC_GPIO;
+    esp32_emac_config.smi_mdio_gpio_num = CONFIG_ETH_MDIO_GPIO;
     esp_eth_mac_t *mac = esp_eth_mac_new_esp32(&esp32_emac_config, &mac_config);
-#if CONFIG_EXAMPLE_ETH_PHY_IP101
+#if CONFIG_ETH_PHY_IP101
     esp_eth_phy_t *phy = esp_eth_phy_new_ip101(&phy_config);
-#elif CONFIG_EXAMPLE_ETH_PHY_RTL8201
+#elif CONFIG_ETH_PHY_RTL8201
     esp_eth_phy_t *phy = esp_eth_phy_new_rtl8201(&phy_config);
-#elif CONFIG_EXAMPLE_ETH_PHY_LAN87XX
+#elif CONFIG_ETH_PHY_LAN87XX
     esp_eth_phy_t *phy = esp_eth_phy_new_lan87xx(&phy_config);
-#elif CONFIG_EXAMPLE_ETH_PHY_DP83848
+#elif CONFIG_ETH_PHY_DP83848
     esp_eth_phy_t *phy = esp_eth_phy_new_dp83848(&phy_config);
-#elif CONFIG_EXAMPLE_ETH_PHY_KSZ80XX
+#elif CONFIG_ETH_PHY_KSZ80XX
     esp_eth_phy_t *phy = esp_eth_phy_new_ksz80xx(&phy_config);
 #endif
 #elif CONFIG_ETH_USE_SPI_ETHERNET
     gpio_install_isr_service(0);
     spi_device_handle_t spi_handle = NULL;
     spi_bus_config_t buscfg = {
-        .miso_io_num = CONFIG_EXAMPLE_ETH_SPI_MISO_GPIO,
-        .mosi_io_num = CONFIG_EXAMPLE_ETH_SPI_MOSI_GPIO,
-        .sclk_io_num = CONFIG_EXAMPLE_ETH_SPI_SCLK_GPIO,
+        .miso_io_num = CONFIG_ETH_SPI_MISO_GPIO,
+        .mosi_io_num = CONFIG_ETH_SPI_MOSI_GPIO,
+        .sclk_io_num = CONFIG_ETH_SPI_SCLK_GPIO,
         .quadwp_io_num = -1,
         .quadhd_io_num = -1,
     };
-    ESP_ERROR_CHECK(spi_bus_initialize(CONFIG_EXAMPLE_ETH_SPI_HOST, &buscfg, SPI_DMA_CH_AUTO));
+    ESP_ERROR_CHECK(spi_bus_initialize(CONFIG_ETH_SPI_HOST, &buscfg, SPI_DMA_CH_AUTO));
 
-#if CONFIG_EXAMPLE_USE_KSZ8851SNL
+#if CONFIG_USE_KSZ8851SNL
     spi_device_interface_config_t devcfg = {
         .mode = 0,
-        .clock_speed_hz = CONFIG_EXAMPLE_ETH_SPI_CLOCK_MHZ * 1000 * 1000,
-        .spics_io_num = CONFIG_EXAMPLE_ETH_SPI_CS_GPIO,
+        .clock_speed_hz = CONFIG_ETH_SPI_CLOCK_MHZ * 1000 * 1000,
+        .spics_io_num = CONFIG_ETH_SPI_CS_GPIO,
         .queue_size = 20
     };
-    ESP_ERROR_CHECK(spi_bus_add_device(CONFIG_EXAMPLE_ETH_SPI_HOST, &devcfg, &spi_handle));
+    ESP_ERROR_CHECK(spi_bus_add_device(CONFIG_ETH_SPI_HOST, &devcfg, &spi_handle));
     /* KSZ8851SNL ethernet driver is based on spi driver */
     eth_ksz8851snl_config_t ksz8851snl_config = ETH_KSZ8851SNL_DEFAULT_CONFIG(spi_handle);
-    ksz8851snl_config.int_gpio_num = CONFIG_EXAMPLE_ETH_SPI_INT_GPIO;
+    ksz8851snl_config.int_gpio_num = CONFIG_ETH_SPI_INT_GPIO;
     esp_eth_mac_t *mac = esp_eth_mac_new_ksz8851snl(&ksz8851snl_config, &mac_config);
     esp_eth_phy_t *phy = esp_eth_phy_new_ksz8851snl(&phy_config);
-#elif CONFIG_EXAMPLE_USE_DM9051
+#elif CONFIG_USE_DM9051
     spi_device_interface_config_t devcfg = {
         .command_bits = 1,
         .address_bits = 7,
         .mode = 0,
-        .clock_speed_hz = CONFIG_EXAMPLE_ETH_SPI_CLOCK_MHZ * 1000 * 1000,
-        .spics_io_num = CONFIG_EXAMPLE_ETH_SPI_CS_GPIO,
+        .clock_speed_hz = CONFIG_ETH_SPI_CLOCK_MHZ * 1000 * 1000,
+        .spics_io_num = CONFIG_ETH_SPI_CS_GPIO,
         .queue_size = 20
     };
-    ESP_ERROR_CHECK(spi_bus_add_device(CONFIG_EXAMPLE_ETH_SPI_HOST, &devcfg, &spi_handle));
+    ESP_ERROR_CHECK(spi_bus_add_device(CONFIG_ETH_SPI_HOST, &devcfg, &spi_handle));
     /* dm9051 ethernet driver is based on spi driver */
     eth_dm9051_config_t dm9051_config = ETH_DM9051_DEFAULT_CONFIG(spi_handle);
-    dm9051_config.int_gpio_num = CONFIG_EXAMPLE_ETH_SPI_INT_GPIO;
+    dm9051_config.int_gpio_num = CONFIG_ETH_SPI_INT_GPIO;
     esp_eth_mac_t *mac = esp_eth_mac_new_dm9051(&dm9051_config, &mac_config);
     esp_eth_phy_t *phy = esp_eth_phy_new_dm9051(&phy_config);
-#elif CONFIG_EXAMPLE_USE_W5500
+#elif CONFIG_USE_W5500
     spi_device_interface_config_t devcfg = {
         .command_bits = 16, // Actually it's the address phase in W5500 SPI frame
         .address_bits = 8,  // Actually it's the control phase in W5500 SPI frame
         .mode = 0,
-        .clock_speed_hz = CONFIG_EXAMPLE_ETH_SPI_CLOCK_MHZ * 1000 * 1000,
-        .spics_io_num = CONFIG_EXAMPLE_ETH_SPI_CS_GPIO,
+        .clock_speed_hz = CONFIG_ETH_SPI_CLOCK_MHZ * 1000 * 1000,
+        .spics_io_num = CONFIG_ETH_SPI_CS_GPIO,
         .queue_size = 20
     };
-    ESP_ERROR_CHECK(spi_bus_add_device(CONFIG_EXAMPLE_ETH_SPI_HOST, &devcfg, &spi_handle));
+    ESP_ERROR_CHECK(spi_bus_add_device(CONFIG_ETH_SPI_HOST, &devcfg, &spi_handle));
     /* w5500 ethernet driver is based on spi driver */
     eth_w5500_config_t w5500_config = ETH_W5500_DEFAULT_CONFIG(spi_handle);
-    w5500_config.int_gpio_num = CONFIG_EXAMPLE_ETH_SPI_INT_GPIO;
+    w5500_config.int_gpio_num = CONFIG_ETH_SPI_INT_GPIO;
     esp_eth_mac_t *mac = esp_eth_mac_new_w5500(&w5500_config, &mac_config);
     esp_eth_phy_t *phy = esp_eth_phy_new_w5500(&phy_config);
 #endif
@@ -233,7 +260,7 @@ static void initialize_ethernet(void)
     esp_eth_config_t config = ETH_DEFAULT_CONFIG(mac, phy);
     config.stack_input = pkt_eth2wifi;
     ESP_ERROR_CHECK(esp_eth_driver_install(&config, &s_eth_handle));
-#if !CONFIG_EXAMPLE_USE_INTERNAL_ETHERNET
+#if !CONFIG_USE_INTERNAL_ETHERNET
     /* The SPI Ethernet module might doesn't have a burned factory MAC address, we cat to set it manually.
        02:00:00 is a Locally Administered OUI range so should not be used except when testing on a LAN under your control.
     */
@@ -252,21 +279,34 @@ static void initialize_wifi(void)
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
     ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
-    wifi_config_t wifi_config = {
-        .ap = {
-            .ssid = CONFIG_EXAMPLE_WIFI_SSID,
-            .ssid_len = strlen(CONFIG_EXAMPLE_WIFI_SSID),
-            .password = CONFIG_EXAMPLE_WIFI_PASSWORD,
-            .max_connection = CONFIG_EXAMPLE_MAX_STA_CONN,
-            .authmode = WIFI_AUTH_WPA_WPA2_PSK,
-            .channel = CONFIG_EXAMPLE_WIFI_CHANNEL // default: channel 1
-        },
-    };
-    if (strlen(CONFIG_EXAMPLE_WIFI_PASSWORD) == 0) {
-        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
-    }
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+
+    #ifdef CONFIG_WLAN_AP_MODE
+        wifi_config_t wifi_config = {
+            .ap = {
+                .ssid = CONFIG_WLAN_SSID,
+                .ssid_len = strlen(CONFIG_WLAN_SSID),
+                .password = CONFIG_WLAN_PASSPHRASE,
+                .max_connection = CONFIG_MAX_STA_CONN,
+                .authmode = WIFI_AUTH_WPA_WPA2_PSK,
+                .channel = CONFIG_WLAN_CHANNEL // default: channel 1
+            },
+        };
+        if (strlen(CONFIG_WLAN_PASSPHRASE) == 0) {
+            wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+        }
+
+        ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+        ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+    #else
+        wifi_config_t wifi_config = {
+            .sta = {
+                .ssid = CONFIG_WLAN_SSID,
+                .password = CONFIG_WLAN_PASSPHRASE,
+            },
+        };
+        ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+        ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    #endif
 }
 
 static esp_err_t initialize_flow_control(void)
